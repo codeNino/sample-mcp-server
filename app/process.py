3c61ab867731aa690ab1817ba35c4abab18ba770
codenino <samuelfadunsin@gmail.com>
2025-12-09 14:13:32 +0100

    commit

--- app/process.py
+++ app/process.py
@@ -1,24 +1,9 @@
-# from fastapi import FastAPI
-
-# server = FastAPI()
-
-# @server.get("/add")
-# def add(a: int, b: int):
-#     return {"result": a + b}
-
-# @server.get("/greet")
-# def greet(name: str):
-#     return {"message": f"Hello, {name}!"}
-
 from fastmcp import FastMCP
 
 mcp = FastMCP("example")
 
-@mcp.tool()
-def add(a: int, b: int):
-    return a + b
 
 @mcp.tool()
-def greet(name: str):
-    return f"Hello {name}"
-
+def onboard_lead(firstname: str, lastname: str, email: str):
+    print(f"Lead acquired with name {firstname} {lastname} and email {email}")
+    return f"{firstname} onboarded successfully"
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import asyncio
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain.agents import create_agent
+from langchain_openai import ChatOpenAI
+from langchain.messages import HumanMessage, AIMessage
 
+from typing import List
+import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_KEY = os.getenv("OPENAI_API_KEY")
 
 async def main():
     # Create MCP client
@@ -20,6 +29,42 @@ async def main():
     tools = await client.get_tools()
     print("Loaded tools:", tools)
 
+    advanced_model = ChatOpenAI(model="gpt-4o", api_key=OPENAI_KEY)
+
+
+    agent = create_agent(
+    model=advanced_model,
+    tools=tools,
+    system_prompt= """
+You are an onboarding assistant. Your job is to collect 3 key things to onboard a new lead:
+1. First name
+2. Last name
+3. Email address
+
+You MUST collect each of these clearly before calling any tool.
+
+When you have all three fields, call the tool with the right inputs:
+
+If email format is invalid, ask the user to re-enter it.
+Be warm, friendly, and conversational.
+"""
+
+)   
+    messages: List = []
+    print("🧠 Lead Onboarding Agent")
+    print("Type 'exit' to quit.\n")
+
+    while True:
+        user_input = input("You: ")
+
+        if user_input.lower() in ["exit", "quit"]:
+            print("Goodbye!")
+            break
+        messages.extend([user_input])
+        response = agent.invoke({"input": messages})["messages"][-1].content
+        print(f"Agent: {response}\n")
+        messages.extend([response])
+        print("messages: ", messages)
 
 if __name__ == "__main__":
     asyncio.run(main())
